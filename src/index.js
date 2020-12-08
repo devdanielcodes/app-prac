5d177a55f333ae0080ec2efc2f28850740b372d9
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-12-08 13:53:03 +0100

    tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './App.css'
 
-const Index = () => {
+const BookList = () => {
+  //functionality
+
+  //DOM
   return(
-    <div>
-      INDEX.JS
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
+const Book = () => {
+  return(
+    <article>
+      <Img />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Img =  () => <img src="https://m.media-amazon.com/images/I/91X6dZem0ZL._AC_UY218_.jpg" alt="" srcset=""/>
+const Title = () => <h4>OSMO - DETECTIVE AGENCY - STEM TOY - FOR IPAD OR FIRE TABLET</h4>
+const Author = () => <h2>STEMMS</h2>
+
+
+
 let root = document.querySelector('#root')
 
-ReactDom.render(<Index />, root)
+ReactDom.render(<BookList />, root)
